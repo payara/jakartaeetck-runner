8877ee49c631a465f31bf79a25549d100518e8cc
Patrik Dudits <patrik.dudits@payara.fish>
2019-08-02 17:18:44 +0000

    Support multiline property overridesDidn't listen to myself and done it in sed

--- functions.sh
+++ functions.sh
@@ -1,9 +1,24 @@
 apply_overrides () {
     OVERRIDE_TEMP=`tempfile`
     ## We create a sed program, that we'll execute against ts.jte.
-    # TODO: Multiline props. Please use something else than sed to implement that ;)
-    sed -n -E "s/^([[:alnum:].]+)=(.+)/s#^\1=.\\\+#\1=\2#/p " ts.override.properties > $OVERRIDE_TEMP
+    # TODO: Non-matching single-line / multiline replacements
+    sed -n -E '
+# single-line to single-line rewrite - create a simple s#prop=anything#prop=override#  
+s/^([[:alnum:].]+)=(.+[^\\])$/s#^\1=.+#\1=\2#/p
+# mutli-line to multi-line rewrite - create a c command for range
+/^([[:alnum:].]+)=(.+\\)/ {
+    # print the change command on the first line
+    s#^([[:alnum:].]+)=.+#/^\1=.+\\\\$/,/[^\\\\]$/ c\\&#
+    :collect
+    N
+    # while backslash is not last, collect into the buffer
+    /[^\\]$/!b collect
+    # and print out all the lines,adding extra backslash
+    s/\\\n/\\\\&/g
+    p
+}
+' ts.override.properties > $OVERRIDE_TEMP
     cat $OVERRIDE_TEMP
-    sed -f $OVERRIDE_TEMP -i $WORKSPACE/bin/ts.jte
+    sed -E -f $OVERRIDE_TEMP -i $WORKSPACE/bin/ts.jte
     rm $OVERRIDE_TEMP
 }
