a22c1f043d4fbc5982c7fa4b6e4fff75055c46bf
Patrik Dudits <patrik.dudits@payara.fish>
2019-08-15 14:43:00 +0000

    Add check for missing bundle files

--- bundles/run_server.sh
+++ bundles/run_server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+check_present() {
+    if [ ! -f $1 ]; then
+       echo -e "\e[33mFile $1 is missing.\e[0m TCK may fail unless you provided custom URL to runner."
+    fi
+}
+
 cd `dirname $0`
+check_present jakartaeetck.zip
+check_present latest-glassfish.zip
+check_present payara-prerelease.zip
 exec python -m SimpleHTTPServer
