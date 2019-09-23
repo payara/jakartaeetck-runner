9612db047d209925774550f4abb5ab625eb2d7d9
Patrik Dudits <patrik.dudits@payara.fish>
2019-09-23 16:11:03 +0000

    Update beanvalidation for CI runs

--- bv/run.sh
+++ bv/run.sh
@@ -8,7 +8,7 @@ PORTING=$SCRIPTPATH/bvtck-porting
 OUTPUT=$PORTING/bundles
 
 rm $PORTING/latest-glassfish.zip
-#rm -rf bvtck-porting/payara5
+rm -rf bvtck-porting/payara5
 
 export WORKSPACE=$SCRIPTPATH/bvtck-porting
 export GF_BUNDLE_URL=$PAYARA_URL
@@ -20,9 +20,18 @@ fi
 
 rm -rf $WORKSPACE/bv-tck-glassfish-porting
 
-bash -x $WORKSPACE/docker/run_bvtck.sh
+bash -x $WORKSPACE/docker/run_bvtck.sh | tee $WORKSPACE/bv.log
 
 TIMESTAMP=`date -Iminutes | tr -d :`
 report=$SCRIPTPATH/../results/bv-$TIMESTAMP.tar.gz
 echo Creating report $report
 tar zcf $report $WORKSPACE/bv-tck-report/ $WORKSPACE/payara5/glassfish/domains/domain1/logs
+
+# override BV_TCK_BUNDLE_URL=http://download.eclipse.org/ee4j/bean-validation/beanvalidation-tck-dist-2.0.5.zip 
+cat > $SCRIPTPATH/../stage_cdi << EOF
+### beanvalidation
+
+\`\`\`
+`sed -n "/Tests run:/,/BUILD/ p" $WORKSPACE/bv.log`
+\`\`\`
+EOF
