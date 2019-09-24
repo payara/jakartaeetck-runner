514696e465d429c21916966d257f91ae4368188f
Patrik Dudits <patrik.dudits@payara.fish>
2019-09-24 11:57:40 +0000

    update BV TCK report folder

--- bv/run.sh
+++ bv/run.sh
@@ -25,7 +25,7 @@ bash -x $WORKSPACE/docker/run_bvtck.sh | tee $WORKSPACE/bv.log
 TIMESTAMP=`date -Iminutes | tr -d :`
 report=$SCRIPTPATH/../results/bv-$TIMESTAMP.tar.gz
 echo Creating report $report
-tar zcf $report $WORKSPACE/bv-tck-report/ $WORKSPACE/payara5/glassfish/domains/domain1/logs
+tar zcf $report $WORKSPACE/bvtck-report/ $WORKSPACE/payara5/glassfish/domains/domain1/logs
 
 # override BV_TCK_BUNDLE_URL=http://download.eclipse.org/ee4j/bean-validation/beanvalidation-tck-dist-2.0.5.zip 
 cat > $SCRIPTPATH/../stage_cdi << EOF
