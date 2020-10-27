c715a72fb15d7d367957c9a9f4705cb50e18738f
Cuba <cuba.stanley@payara.fish>
2020-10-27 15:54:06 +0000

    Auto Download Artifacts for Local Runs

--- bundles/run_server.sh
+++ bundles/run_server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 check_present() {
     if [ ! -f $1 ]; then
-       echo -e "\e[33mFile $1 is missing.\e[0m TCK may fail unless you provided custom URL to runner."
+       #echo -e "\e[33mFile $1 is missing.\e[0m TCK may fail unless you provided custom URL to runner."
+       echo "Downloading required TCK artifact $1"
+       ./download.sh
     fi
 }
 
@@ -9,7 +11,8 @@ cd `dirname $0`
 check_present jakartaeetck.zip
 check_present latest-glassfish.zip
 check_present payara-prerelease.zip
-check_present cdi-tck-2.0.6-dist.zip
+check_present cdi-tck-3.0.0-dist.zip
+check_present bv-tck-3.0.0-dist.zip
 check_present jakarta.inject-tck-1.0-bin.zip
 check_present javadb.zip
 check_present ejbtimer_derby.sql
