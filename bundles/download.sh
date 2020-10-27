1686ca413ba08475560466eb5a398a66cd84b106
Cuba <cuba.stanley@payara.fish>
2020-10-27 15:54:06 +0000

    Auto Download Artifacts for Local Runs

--- bundles/download.sh
+++ bundles/download.sh
@@ -1,9 +1,15 @@
 if [ ! -f jakartaeetck.zip ]; then
-  wget --read-timeout=20 http://download.eclipse.org/jakartaee/platform/8/eclipse-jakartaeetck-8.0.1.zip -O jakartaeetck.zip
+  wget --read-timeout=20 https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee9/milestones/jakartaeetck-9.0.0-M1.zip -O jakartaeetck.zip
 fi
-if [ ! -f cdi-tck-2.0.6-dist.zip ]; then
-  wget http://download.eclipse.org/ee4j/cdi/cdi-tck-2.0.6-dist.zip
+if [ ! -f cdi-tck-3.0.0-dist.zip ]; then
+  wget https://download.eclipse.org/ee4j/cdi/3.0/cdi-tck-3.0.0-dist.zip -O cdi-tck-3.0.0-dist.zip
 fi
 if [ ! -f jakarta.inject-tck-1.0-bin.zip ]; then
-  wget http://download.eclipse.org/ee4j/cdi/jakarta.inject-tck-1.0-bin.zip
+  wget https://download.eclipse.org/ee4j/cdi/inject/2.0/jakarta.inject-tck-2.0.1-bin.zip -O jakarta-inject-tck-2.0.1-bin.zip
+fi
+if [ ! -f bv-tck-3.0.0-dist.zip ]; then
+	wget https://download.eclipse.org/ee4j/bean-validation/3.0/beanvalidation-tck-dist-3.0.0.zip -O bv-tck-3.0.0-dist.zip
+fi
+if [ ! -f latest-glassfish.zip ]; then
+	wget https://download.eclipse.org/ee4j/glassfish/glassfish-6.0.0-M1.zip -O latest-glassfish.zip
 fi
