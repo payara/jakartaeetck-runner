6d620702241f5d56a253374c7faf374dcf0fb627
Cuba <cuba.stanley.guitar@gmail.com>
2021-02-10 16:16:04 +0000

    Correct CDI version

--- bundles/download.sh
+++ bundles/download.sh
@@ -2,7 +2,7 @@ if [ ! -f jakartaeetck.zip ]; then
   wget --read-timeout=20 https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee9/promoted/jakartaeetck-9.0.0.zip -O jakartaeetck.zip
 fi
 if [ ! -f cdi-tck-3.0.2-dist.zip ]; then
-  wget https://download.eclipse.org/ee4j/cdi/3.0/cdi-tck-3.0.2-dist.zip -O cdi-tck-3.0.2-dist.zip
+  wget https://download.eclipse.org/ee4j/cdi/3.0/cdi-tck-3.0.1-dist.zip -O cdi-tck-3.0.2-dist.zip
 fi
 if [ ! -f jakarta.inject-tck-2.0.1-bin.zip ]; then
   wget https://download.eclipse.org/ee4j/cdi/inject/2.0/jakarta.inject-tck-2.0.1-bin.zip -O jakarta-inject-tck-2.0.1-bin.zip
