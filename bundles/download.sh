399a53c354d69324473edddef9d6f2e1d85a4b86
Andrew Pielage <pandrex247@hotmail.com>
2022-02-01 14:42:51 +0000

    FISH-5982 Use 3.0.3 - they removed 3.0.2
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- bundles/download.sh
+++ bundles/download.sh
@@ -1,8 +1,8 @@
 if [ ! -f jakartaeetck.zip ]; then
   wget --read-timeout=20 https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee9/promoted/jakartaeetck-9.1.0.zip -O jakartaeetck.zip
 fi
-if [ ! -f cdi-tck-3.0.2-dist.zip ]; then
-  wget https://download.eclipse.org/ee4j/cdi/3.0/cdi-tck-3.0.2-dist.zip -O cdi-tck-3.0.2-dist.zip
+if [ ! -f cdi-tck-3.0.3-dist.zip ]; then
+  wget https://download.eclipse.org/ee4j/cdi/3.0/cdi-tck-3.0.3-dist.zip -O cdi-tck-3.0.3-dist.zip
 fi
 if [ ! -f jakarta-inject-tck-2.0.1-bin.zip ]; then
   wget https://download.eclipse.org/ee4j/cdi/inject/2.0/jakarta.inject-tck-2.0.1-bin.zip -O jakarta.inject-tck-2.0.1-bin.zip
