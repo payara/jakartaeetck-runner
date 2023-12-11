fe50306ac3b83604c933b550714ada654efa42c4
Petr Aubrecht <aubrecht@asoftware.cz>
2023-12-11 19:52:28 +0100

    FISH-8073 upgrade TCK to 8.0.3

--- bundles/download.sh
+++ bundles/download.sh
@@ -1,5 +1,5 @@
 if [ ! -f jakartaeetck.zip ]; then
-  wget --read-timeout=20 http://download.eclipse.org/jakartaee/platform/8/eclipse-jakartaeetck-8.0.1.zip -O jakartaeetck.zip
+  wget --read-timeout=20 http://download.eclipse.org/jakartaee/platform/8/jakarta-jakartaeetck-8.0.3.zip -O jakartaeetck.zip
 fi
 if [ ! -f cdi-tck-2.0.6-dist.zip ]; then
   wget http://download.eclipse.org/ee4j/cdi/cdi-tck-2.0.6-dist.zip
