e5887da3ae4f75cb2c0c9cf766e034dfb208e819
Andrew Pielage <pandrex247@hotmail.com>
2025-06-30 12:14:27 +0100

    FISH-11490 Testing fix for connector TCK
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- jakartaeetck.sh
+++ jakartaeetck.sh
@@ -462,7 +462,9 @@ echo 'permission java.net.SocketPermission "*", "listen";' >> ${VI_SERVER_POLICY
 echo 'permission java.net.SocketPermission "*", "accept";' >> ${VI_SERVER_POLICY_FILE}
 echo 'permission java.io.FilePermission       "<<ALL FILES>>", "write,read";' >> ${VI_SERVER_POLICY_FILE}
 echo 'permission org.apache.derby.security.SystemPermission "engine", "usederbyinternals";' >> ${VI_SERVER_POLICY_FILE}
-echo 'permission jakarta.xml.ws.WebServicePermission "CTSPermission3_name";' >> ${VI_SERVER_POLICY_FILE}
+if [[ "$JDK" == "JDK17" || "$JDK" == "jdk17" ]]; then
+  echo 'permission jakarta.xml.ws.WebServicePermission "CTSPermission3_name";' >> ${VI_SERVER_POLICY_FILE}
+fi
 echo '};' >> ${VI_SERVER_POLICY_FILE}
 
 VI_APPCLIENT_POLICY_FILE=${CTS_HOME}/vi/$GF_VI_TOPLEVEL_DIR/glassfish/lib/appclient/client.policy
