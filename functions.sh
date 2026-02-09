554fe1d31aea601e461fc46324ef7ed224c3bfa5
Andrew Pielage <pandrex247@hotmail.com>
2026-02-09 10:00:56 +0000

    FISH-12956 Set Jakarta Activation URLs to Maven Central
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- functions.sh
+++ functions.sh
@@ -78,6 +78,12 @@ init_urls () {
     if [ -z "$DERBY_URL" ]; then
         DERBY_URL=$BASE_URL/javadb.zip
     fi
+    if [ -z "$ACTIVATION_BUNDLE_URL" ]; then
+        ACTIVATION_BUNDLE_URL=https://repo1.maven.org/maven2/com/sun/activation/jakarta.activation/2.0.0/jakarta.activation-2.0.0.jar
+    fi
+    if [ -z "$ANGUS_BUNDLE_URL" ]; then
+        ANGUS_BUNDLE_URL=https://repo1.maven.org/maven2/org/eclipse/angus/angus-activation/1.1.0/angus-activation-1.1.0.jar
+    fi
 }
 
 make_stage_log () {
